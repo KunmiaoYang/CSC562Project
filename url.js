609dbe84e89f24f82f1bbd25ac447c9b502b8755
kyang12 <kyang12@ncsu.edu>
2019-03-08 01:13:10 -0500

    modify URL

--- url.js
+++ url.js
@@ -1,33 +1,31 @@
 var URL = function () {
     let textureAddress = 'https://kunmiaoyang.github.io/textures/';
-    let professorAddress = 'https://ncsucg4games.github.io/prog2/';
-    let baseAddress = 'https://kunmiaoyang.github.io/MissileCommand/';
+    let modelAddress = 'https://kunmiaoyang.github.io/CSC562Models/';
     return {
-        rooms: professorAddress + 'rooms.json',
-        triangles: professorAddress + 'triangles.json',
-        ellipsoids: baseAddress + 'model/ellipsoids.json',
-        sphere: professorAddress + 'spheres.json',
-        // lights: baseAddress + 'model/lights.json',
-        cityModel: baseAddress + 'model/city.dae',
-        batteryModel: baseAddress + 'model/battery.dae',
-        attackMissileModel: baseAddress + 'model/AttackMissile.dae',
-        defenseMissileModel: baseAddress + 'model/DefenseMissile.dae',
-        UFOModel: baseAddress + 'model/UFO.dae',
+        rooms: modelAddress + 'rooms.json',
+        triangles: modelAddress + 'triangles.json',
+        ellipsoids: modelAddress + 'ellipsoids.json',
+        sphere: modelAddress + 'spheres.json',
+        cityModel: modelAddress + 'city.dae',
+        batteryModel: modelAddress + 'battery.dae',
+        attackMissileModel: modelAddress + 'AttackMissile.dae',
+        defenseMissileModel: modelAddress + 'DefenseMissile.dae',
+        UFOModel: modelAddress + 'UFO.dae',
         texture: {
             'floor': textureAddress + 'floor.jpg',
             'background': textureAddress + 'background.jpg',
             'ceiling': textureAddress + 'ceiling.jpg',
 
-            'abe.png': professorAddress + 'abe.png',
-            // 'billie.jpg': professorAddress + 'billie.jpg',
-            // 'earth.png': professorAddress + 'earth.png',
-            'glass.gif': professorAddress + 'glass.gif',
-            // 'leaf.small.png': professorAddress + 'leaf.small.png',
-            // 'retro.jpg': professorAddress + 'retro.jpg',
-            // 'rocktile.jpg': professorAddress + 'rocktile.jpg',
-            // 'sky.jpg': professorAddress + 'sky.jpg',
-            // 'stars.jpg': professorAddress + 'stars.jpg',
-            // 'tree.png': professorAddress + 'tree.png',
+            'abe.png': textureAddress + 'abe.png',
+            // 'billie.jpg': textureAddress + 'billie.jpg',
+            // 'earth.png': textureAddress + 'earth.png',
+            'glass.gif': textureAddress + 'glass.gif',
+            // 'leaf.small.png': textureAddress + 'leaf.small.png',
+            // 'retro.jpg': textureAddress + 'retro.jpg',
+            // 'rocktile.jpg': textureAddress + 'rocktile.jpg',
+            // 'sky.jpg': textureAddress + 'sky.jpg',
+            // 'stars.jpg': textureAddress + 'stars.jpg',
+            // 'tree.png': textureAddress + 'tree.png',
         }
     };
 }();
